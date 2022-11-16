6dc01d661235df49d00b4b39143fcdb0b822f043
GabrielModin <gabriel.barzen@mailfence.com>
2022-11-16 12:19:37 +0100

    Running

--- L1/src/main/java/com/eudycontreras/othello/application/OthelloResources.java
+++ L1/src/main/java/com/eudycontreras/othello/application/OthelloResources.java
@@ -17,36 +17,36 @@ import java.util.Objects;
  */
 public class OthelloResources {
 
-	public static final String IMAGE_SOURCE_DIRECTORY = "res"; //TODO correct path
+
 	
-	public static final Image LOGO = new Image(loadResource("res/logo.png"));
+	public static final Image LOGO = new Image(loadResource("logo.png"));
 	
-	public static final Image BOARD_TEXTURE_1 = new Image(loadResource("res/board_texture_1.jpg"));
+	public static final Image BOARD_TEXTURE_1 = new Image(loadResource("board_texture_1.jpg"));
 	
-	public static final Image BOARD_TEXTURE_2 = new Image(loadResource("res/board_texture_2.jpg"));
+	public static final Image BOARD_TEXTURE_2 = new Image(loadResource("board_texture_2.jpg"));
 	
-	public static final Image CREATOR_PROFILE = new Image(loadResource("res/eudy_contreras.jpg"));
+	public static final Image CREATOR_PROFILE = new Image(loadResource("eudy_contreras.jpg"));
 	
 	public static final Image TEXTURE[] = new Image[]{
-			new Image(loadResource("res/texture_16.jpg")),
-			new Image(loadResource("res/texture_0.jpg")),
-			new Image(loadResource("res/texture_1.jpg")),
-			new Image(loadResource("res/texture_2.jpg")),
-			new Image(loadResource("res/texture_3.jpg")),
-			new Image(loadResource("res/texture_4.jpg")),
-			new Image(loadResource("res/texture_5.jpg")),
-			new Image(loadResource("res/texture_6.jpg")),
-			new Image(loadResource("res/texture_7.jpg")),
-			new Image(loadResource("res/texture_8.jpg")),
-			new Image(loadResource("res/texture_10.jpg")),
-			new Image(loadResource("res/texture_11.jpg")),
-			new Image(loadResource("res/texture_12.jpg")),
-			new Image(loadResource("res/texture_13.jpg")),
+			new Image(loadResource("texture_16.jpg")),
+			new Image(loadResource("texture_0.jpg")),
+			new Image(loadResource("texture_1.jpg")),
+			new Image(loadResource("texture_2.jpg")),
+			new Image(loadResource("texture_3.jpg")),
+			new Image(loadResource("texture_4.jpg")),
+			new Image(loadResource("texture_5.jpg")),
+			new Image(loadResource("texture_6.jpg")),
+			new Image(loadResource("texture_7.jpg")),
+			new Image(loadResource("texture_8.jpg")),
+			new Image(loadResource("texture_10.jpg")),
+			new Image(loadResource("texture_11.jpg")),
+			new Image(loadResource("texture_12.jpg")),
+			new Image(loadResource("texture_13.jpg")),
 	};
 	
 
 	public static String loadResource(String image) {
-		String url = IMAGE_SOURCE_DIRECTORY + image;
+		String url = OthelloResources.class.getClassLoader().getResource(image).toString();
 		return url;
 	}
 }
