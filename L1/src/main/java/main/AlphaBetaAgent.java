8f38260a642e94548c13e5ab5a4573ba0a36b498
nicholasnarvell <56867731+nicholasnarvell@users.noreply.github.com>
2023-02-23 15:04:30 +0100

    removed unnecessary lines in alphabetaagent and removed node class

--- L1/src/main/java/main/AlphaBetaAgent.java
+++ L1/src/main/java/main/AlphaBetaAgent.java
@@ -37,42 +37,14 @@ public class AlphaBetaAgent extends Agent{
 		// TODO Auto-generated constructor stub
 	}
 
-
-
 	/**
 	 * Delete the content of this method and Implement your logic here!
 	 */
 	@Override
 	public AgentMove getMove(GameBoardState gameState) {
-
-
-
-		List<ObjectiveWrapper> allValidMoves = AgentController.getAvailableMoves(gameState, playerTurn);
-		List<MoveWrapper> moves = new ArrayList<>();
-		for (ObjectiveWrapper move : allValidMoves) {
-
-			AgentMove ex = new MoveWrapper(move);
-			moves.add(new MoveWrapper(move));
-
-		}
-
 		int depth = 6;
-
 		GameBoardState dt = GameTreeUtility.buildDecisionTree(gameState,depth,2500);
 		GameTreeUtility.printBoard(dt.getCells());
-		//int outAlfa = alfabetagrej(dt, depth, Integer.MIN_VALUE, Integer.MAX_VALUE, false);
-		int outAlfa = abp(dt,depth,Integer.MIN_VALUE,Integer.MAX_VALUE,true);
-		MoveWrapper wrap = null;
-		for (GameBoardState childState : gameState.getChildStates()) {
-			System.out.println(childState.getBlackCount() + " == " + outAlfa);
-			if(childState.getBlackCount() == outAlfa) {
-				System.out.println("BF moves");
-				System.out.println(childState.getLeadingMove());
-				wrap = new MoveWrapper(childState.getLeadingMove());
-			}
-		}
-		System.out.println("----------");
-		//abp(gameState,depth,Integer.MIN_VALUE,Integer.MIN_VALUE,true);
 
 		AlphaBetaPruning moveFinder = new AlphaBetaPruning();
 
@@ -86,73 +58,6 @@ public class AlphaBetaAgent extends Agent{
 			return new MoveWrapper(bestState.getLeadingMove());
 		} else {
 			return getExampleMove(gameState);
-		}
-	}
-
-
-	private int abp (GameBoardState gbs, int depth, int alpha, int beta, boolean max) {
-//		System.out.println("Depth is : " + depth);
-		if (depth == 0 || gbs.isTerminal()) {
-//			System.out.println("no, it is working, promise");
-//			System.out.println("num black :" + gbs.getBlackCount());
-			System.out.println("num black : " + gbs.getBlackCount() + ", depth :" + depth );
-			return gbs.getBlackCount();
-		}
-		int value = max ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-		//System.out.println(" pre val: " + value + ", black : " + max);
-		for (GameBoardState child : gbs.getChildStates()) {
-			value = max ? Math.max(value,abp(child,depth-1,alpha,beta, false)) : Math.min(value,abp(child,depth-1,alpha,beta, true));
-			//System.out.println("post val: " + value + ", black : " + max);
-			if (max) {
-				if (value >= beta) {
-					break;
-				}
-			} else {
-				if (value <= alpha) {
-					break;
-				}
-			}
-			if (!max) {
-				alpha = Math.max(alpha,value);
-			} else {
-				beta = Math.min(beta,value);
-			}
-		}
-
-		return value;
-	}
-
-	private int alfabetagrej(GameBoardState bst, int depth, int alfa, int beta, boolean max) {
-		int value;
-		if(depth == 0 || bst.isTerminal()) {
-			return bst.getBlackCount();
-
-		}
-		if(max){
-			value = Integer.MIN_VALUE;
-			for (GameBoardState childState : bst.getChildStates()) {
-				value = Math.max(value, alfabetagrej(childState, depth-1, alfa, beta, false));
-				if(value >= beta) {
-					break;
-				}
-				alfa = Math.max(alfa, value);
-			}
-			System.out.println("value is : " + value);
-
-			return value;
-		} else {
-			value = Integer.MAX_VALUE;
-			for (GameBoardState childState : bst.getChildStates()) {
-				value = Math.min(value, alfabetagrej(childState, depth-1, alfa, beta, true));
-
-				if(value <= alfa) {
-					break;
-				}
-				beta = Math.min(beta, value);
-			}
-			System.out.println("value is : " + value);
-
-			return value;
 		}
 	}
 
